7d57510b3cb21aebf62ca1f90ab10c5d73287033
Kevin <rozariokevin247@gmail.com>
2026-01-01 21:21:05 -0500

    added a simple layout for the upload page

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -1,8 +1,31 @@
 import { UploadCSV } from '../components/UploadCSV';
-import { useNavigate } from 'react-router-dom';
+import { AppShell, Center, Title } from "@mantine/core"
+import { useMantineTheme } from '@mantine/core';
 
 export function UploadPage() {
+  const theme = useMantineTheme();
   return (
-    <UploadCSV />
+      <AppShell
+      header={{height: 60}}
+      >
+        <AppShell.Header style={{
+          background: theme.colors.dark[7],
+          color: theme.colors.dark[1]
+        }}>
+            <Center>
+                <Title order={1}>Expense Tracker</Title>
+            </Center>
+        </AppShell.Header>
+
+        <AppShell.Main style={{
+          backgroundColor: theme.colors.dark[7],
+          color: theme.colors.dark[1]
+        }}>
+          <Center> 
+            <UploadCSV />
+          </Center>
+        </AppShell.Main>
+
+      </AppShell>
   );
 }
